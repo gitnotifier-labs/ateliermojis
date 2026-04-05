53f2b6b7804a28a976db2702f2f9a2444bc89192
Francois Le Pape <32224751+Lp-Francois@users.noreply.github.com>
2026-04-05 12:42:59 +0200

    chore: upgrade React 19 and migrate router package

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
